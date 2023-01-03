f5b5f11de2b294f0cf45401e2a63d7585613cc53
Linn Strand <linnber@msn.com>
2023-01-03 10:13:33 +0100

    linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
-import * as d3 from "d3";
-import { Graph } from "./Graph";
-import testdata from "./testdata.json";
+import React, { useEffect, useState } from 'react';
+import * as d3 from 'd3';
+import { Graph } from './Graph';
+import testdata from './testdata.json';
 
 function App() {
   const [data, setData] = useState(undefined);
   useEffect(() => {
     if (data) return;
     const readData = async () => {
-      const response = await fetch("testdata.csv");
+      const response = await fetch('testdata.csv');
       const reader = response.body.getReader();
       const result = await reader.read();
-      const decoder = new TextDecoder("utf-8");
+      const decoder = new TextDecoder('utf-8');
       const csv = await decoder.decode(result.value);
       const table = d3.csvParse(csv);
       setData(table);
@@ -20,7 +20,7 @@ function App() {
     readData();
   }, []);
 
-  return <div className="App">{<Graph />}</div>;
+  return <div className="App">{<Graph data={testdata} />}</div>;
 }
 
 export default App;
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,27 +1,27 @@
-import React, { useRef, useState } from "react";
-import * as d3 from "d3";
-import data from "./testdata.json";
+import React, { useMemo, useRef, useState } from 'react';
+import * as d3 from 'd3';
 
 const SIZE = 975;
-const RADIUS = SIZE / 2;
+const RADIUS = SIZE / 6;
 
 interface Data {
   name: string;
   value?: number;
+  children?: Data[];
 }
 
-export const Graph = () => {
+export const Graph = ({ data }: { data: Data }) => {
   const svgRef = useRef<SVGSVGElement>(null);
-  const [viewBox, setViewBox] = useState("0,0,0,0");
-  const [root, setRoot] = useState(() => {
-    const p = d3.partition<Data>().size([2 * Math.PI, RADIUS])(
+  const [viewBox, setViewBox] = useState('0,0,0,0');
+  const root = useMemo(() => {
+    const p = d3.partition<Data>().size([2 * Math.PI, RADIUS * 4])(
       d3
         .hierarchy(data)
         .sum((d) => d.value)
         .sort((a, b) => b.value - a.value)
     );
     return p.descendants().filter((d) => d.depth < 3);
-  });
+  }, [data]);
 
   React.useEffect(() => {
     if (!svgRef.current) return;
@@ -35,13 +35,11 @@ export const Graph = () => {
       .startAngle((d) => d.x0)
       .endAngle((d) => d.x1)
       .padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
-      .padRadius(RADIUS / 3)
+      .padRadius(RADIUS)
       .innerRadius((d) => d.y0)
       .outerRadius((d) => d.y1 - 1);
 
-    const color = d3.scaleOrdinal(
-      d3.quantize(d3.interpolateRainbow, data.children.length + 1)
-    );
+    const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1));
 
     function arcVisible(d) {
       return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
@@ -52,43 +50,38 @@ export const Graph = () => {
     }
 
     if (!svgRef.current) return;
-    svgRef.current.innerHTML = "";
+    svgRef.current.innerHTML = '';
     const svg = d3.select(svgRef.current);
     svg
-      .append("g")
-      .selectAll("path")
-      .data(root)
-      .join("path")
-      .attr("fill", (d) => {
+      .append('g')
+      .selectAll('path')
+      .data(root.slice(1))
+      .join('path')
+      .attr('fill', (d) => {
         while (d.depth > 1) d = d.parent;
         return color(d.data.name);
       })
-      .attr("d", (d) => arc(d))
-      .attr("fill", (d) => {
+      .attr('d', (d) => arc(d))
+      .attr('fill', (d) => {
         while (d.depth > 1) d = d.parent;
         return color(d.data.name);
       })
-      .attr("fill-opacity", (d) => (d.children ? 0.6 : 0.4));
+      .attr('fill-opacity', (d) => (d.children ? 0.6 : 0.4));
 
     svg
-      .append("g")
-      .selectAll("text")
+      .append('g')
+      .selectAll('text')
       .data(root.filter((d) => ((d.y0 + d.y1) / 2) * (d.x1 - d.x0) > 10))
-      .join("text")
-      .attr("text-anchor", "middle")
-      .attr("alignment-baseline", "middle")
-      .attr("fill-opacity", (d) => labelVisible(d))
-      .attr(
-        "font-size",
-        (d) => `${Math.min(((d.y0 + d.y1) / 2) * (d.x1 - d.x0) - 6, 10)}px`
-      )
-      .attr("transform", (d: d3.HierarchyRectangularNode<Data>) => {
+      .join('text')
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle')
+      .attr('fill-opacity', (d) => labelVisible(d))
+      .attr('font-size', (d) => `${Math.min(((d.y0 + d.y1) / 2) * (d.x1 - d.x0) - 6, 10)}px`)
+      .attr('transform', (d: d3.HierarchyRectangularNode<Data>) => {
         if (!d.depth || !d.data.name) return;
         const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
         const y = (d.y0 + d.y1) / 2;
-        return `rotate(${x - 90}) translate(${y},0) rotate(${
-          x < 180 ? 0 : 180
-        })`;
+        return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
       })
       .text((d) => d.data.name);
   }, [root]);
